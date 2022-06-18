f67b5f970b7c37052e95e9ad8a5d5513c48dfb47
Pablo Gonzalez <pablo_ignacio18@hotmail.cl>
2022-06-18 14:08:26 -0400

    Database
    Connection to the database
    
    By: Matías Vergara - Pablo Gonzalez

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+require("dotenv").config();
+const moongoose = require("mongoose");
 
 // Router Imports
 const testRouter = require('./routes/testRouter');
@@ -9,6 +11,15 @@ const testRouter = require('./routes/testRouter');
 // App Settings and Middlewares
 app.use(cors({ origin: '*' }));
 app.use(express.json());
+
+//database connection
+moongoose
+  .connect(process.env.DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connect to the database"))
+  .catch((err) => console.log(err));
 
 // Configuring port
 const port = process.env.PORT || 9000;
