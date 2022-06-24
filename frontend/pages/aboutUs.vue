14a926ccf90c2b0b5b83043d730181873f2dec69
Serllet <fuentesserllet@gmail.com>
2022-06-24 02:09:52 -0400

    Avance about us

--- frontend/pages/aboutUs.vue
+++ frontend/pages/aboutUs.vue
@@ -1,5 +1,47 @@
 <template>
-  <h1>NOSOTROS EN INGLES</h1>
+   <section class="ftco-section">
+      <div class="container">
+        <div class="row justify-content-center mb-5 pb-5">
+          <div class="col-md-7 text-center heading-section ftco-animate">
+            <h2 class="mb-4">Nosotros </h2>
+            <p>Sobre nosotros ....</p>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-4 mb-5 ftco-animate">
+            <div class="block-10">
+              <div class="person-info mb-2">
+                <span class="name">Nombre 1</span>
+                <span class="position">Que hace</span>
+              </div>
+              <img src="images/person_1.jpg" alt="" class="img-fluid mb-3">
+              <p>Informacion de persona 1</p>
+            </div>
+          </div>
+          <div class="col-md-4 mb-5 ftco-animate">
+            <div class="block-10">
+              <div class="person-info mb-2">
+                <span class="name">Persona 2</span>
+                <span class="position">Que hace</span>
+              </div>
+              <img src="images/person_3.jpg" alt="" class="img-fluid mb-3">
+              <p>Informacion persona 2</p>
+            </div>
+          </div>
+          <div class="col-md-4 mb-5 ftco-animate">
+            <div class="block-10">
+              <div class="person-info mb-2">
+                <span class="name">Persona 3</span>
+                <span class="position">Que hace</span>
+              </div>
+              <img src="images/person_2.jpg" alt="" class="img-fluid mb-3">
+              <p>Informacion persona 3</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
 </template>
 
 <script>
