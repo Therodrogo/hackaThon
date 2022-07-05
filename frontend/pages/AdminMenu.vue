90b400cc343c08d52f89521b718aadbeae66de9e
Daniel <dsalazar15@alumnos.utalca.cl>
2022-07-04 21:26:40 -0400

    Botones que muestran cosas.
    Se implementaron 3 botones que muestran cosas, y se valido que no se puedan mostrar mas de 1 a la vez,
    trabajo de Daniel salazar, y Juan farias.

--- frontend/pages/AdminMenu.vue
+++ frontend/pages/AdminMenu.vue
@@ -9,42 +9,122 @@
                             Admin
                         </div>
                         <div class="justify-center text-center mb-3">
-                            <v-btn rounded elevation="2" color="#00CCB1">
+                            <v-btn @click="createEventMethod" rounded elevation="2" color="#00CCB1">
                                 Crear Evento
+                            </v-btn>
+                        </div>
+                        <div class="justify-center text-center mb-3 mt-15">
+                            <v-btn @click="createAdminHMethod" rounded elevation="2" color="#00CCB1">
+                                Crear Algo
+                            </v-btn>
+                        </div>
+                        <div class="justify-center text-center mb-3 mt-15">
+                            <v-btn @click="createAnuncio" rounded elevation="2" color="#00CCB1">
+                                Crear Anuncio
                             </v-btn>
                         </div>
                     </v-col>
 
-                    <v-col cols="6">
+                    <v-col v-if="createEvent" cols="6">
                         <v-container>
-                <v-col sm="20" >
-                    <v-text-field label="ID" prepend-icon="mdi-key"></v-text-field>
-                </v-col>
-                <v-col sm="20">
-                    <v-text-field label="Nombre" prepend-icon="mdi-account-circle"></v-text-field>
-                </v-col>
-                <v-col sm="20">
-                    <v-textarea label="Descripcion" value="" prepend-icon="mdi-comment-text-outline"></v-textarea>
-                </v-col>
-                <v-col sm="20">
-                    <v-text-field label="Lugar" prepend-icon="mdi-map-marker-radius"></v-text-field>
-                </v-col>
-                <v-col sm="20">
-                    <v-text-field label="Fecha de inicio" prepend-icon="mdi-calendar"></v-text-field>
-                </v-col>
-                <v-col sm="20" >
-                    <v-text-field label="Fecha de termino" prepend-icon="mdi-calendar-check"></v-text-field>
-                </v-col>
-                <v-col sm="20">
-                    <v-text-field label="Limite de integrantes" prepend-icon="mdi-account-multiple"></v-text-field>
-                </v-col>
-                <v-col sm="20" >
-                    <v-text-field label="URL banner" prepend-icon="mdi-file-image"></v-text-field>
-                </v-col>
-                <v-btn rounded elevation="2" color="#00CCB1">
-                            Confirmar evento
-                        </v-btn>
-            </v-container>
+                            <v-col sm="20">
+                                <v-text-field label="ID" prepend-icon="mdi-key"></v-text-field>
+                            </v-col>
+                            <v-col sm="20">
+                                <v-text-field label="Nombre" prepend-icon="mdi-account-circle"></v-text-field>
+                            </v-col>
+                            <v-col sm="20">
+                                <v-textarea label="Descripcion" value="" prepend-icon="mdi-comment-text-outline">
+                                </v-textarea>
+                            </v-col>
+                            <v-col sm="20">
+                                <v-text-field label="Lugar" prepend-icon="mdi-map-marker-radius"></v-text-field>
+                            </v-col>
+                            <v-col sm="20">
+                                <v-text-field label="Fecha de inicio" prepend-icon="mdi-calendar"></v-text-field>
+                            </v-col>
+                            <v-col sm="20">
+                                <v-text-field label="Fecha de termino" prepend-icon="mdi-calendar-check"></v-text-field>
+                            </v-col>
+                            <v-col sm="20">
+                                <v-text-field label="Limite de integrantes" prepend-icon="mdi-account-multiple">
+                                </v-text-field>
+                            </v-col>
+                            <v-col sm="20">
+                                <v-text-field label="URL banner" prepend-icon="mdi-file-image"></v-text-field>
+                            </v-col>
+                            <v-btn rounded elevation="2" color="#00CCB1">
+                                Confirmar evento
+                            </v-btn>
+                        </v-container>
+                    </v-col>
+
+                    <v-col v-if="adminholder" cols="6">
+                        <v-container>
+                            <v-col sm="20">
+                                <v-text-field label="Admin" prepend-icon="mdi-key"></v-text-field>
+                            </v-col>
+                            <v-col sm="20">
+                                <v-text-field label="Nombre" prepend-icon="mdi-account-circle"></v-text-field>
+                            </v-col>
+                            <v-col sm="20">
+                                <v-textarea label="Descripcion" value="" prepend-icon="mdi-comment-text-outline">
+                                </v-textarea>
+                            </v-col>
+                            <v-col sm="20">
+                                <v-text-field label="Lugar" prepend-icon="mdi-map-marker-radius"></v-text-field>
+                            </v-col>
+                            <v-col sm="20">
+                                <v-text-field label="Fecha de inicio" prepend-icon="mdi-calendar"></v-text-field>
+                            </v-col>
+                            <v-col sm="20">
+                                <v-text-field label="Fecha de termino" prepend-icon="mdi-calendar-check"></v-text-field>
+                            </v-col>
+                            <v-col sm="20">
+                                <v-text-field label="Limite de integrantes" prepend-icon="mdi-account-multiple">
+                                </v-text-field>
+                            </v-col>
+                            <v-col sm="20">
+                                <v-text-field label="URL banner" prepend-icon="mdi-file-image"></v-text-field>
+                            </v-col>
+                            <v-btn rounded elevation="2" color="#00CCB1">
+                                Confirmar evento
+                            </v-btn>
+                        </v-container>
+                    </v-col>
+
+                    <v-col v-if="anuncio" cols="6">
+                        <v-container>
+                            <v-col sm="20">
+                                <v-text-field label="Anuncio" prepend-icon="mdi-key"></v-text-field>
+                            </v-col>
+                            <v-col sm="20">
+                                <v-text-field label="Nombre" prepend-icon="mdi-account-circle"></v-text-field>
+                            </v-col>
+                            <v-col sm="20">
+                                <v-textarea label="Descripcion" value="" prepend-icon="mdi-comment-text-outline">
+                                </v-textarea>
+                            </v-col>
+                            <v-col sm="20">
+                                <v-text-field label="Lugar" prepend-icon="mdi-map-marker-radius"></v-text-field>
+                            </v-col>
+                            <v-col sm="20">
+                                <v-text-field label="Fecha de inicio" prepend-icon="mdi-calendar"></v-text-field>
+                            </v-col>
+                            <v-col sm="20">
+                                <v-text-field label="Fecha de termino" prepend-icon="mdi-calendar-check"></v-text-field>
+                            </v-col>
+                            <v-col sm="20">
+                                <v-text-field label="Limite de integrantes" prepend-icon="mdi-account-multiple">
+                                </v-text-field>
+                            </v-col>
+                            <v-col sm="20">
+                                <v-text-field label="URL banner" prepend-icon="mdi-file-image"></v-text-field>
+                            </v-col>
+                            <v-btn rounded elevation="2" color="#00CCB1">
+                                Confirmar evento
+                            </v-btn>
+                        </v-container>
                     </v-col>
 
                 </v-row>
@@ -52,3 +132,56 @@
         </v-main>
     </v-app>
 </template>
+
+<script>
+import { placeholder } from '@babel/types'
+
+export default {
+    data() {
+        return {
+            createEvent: false,
+            adminholder: false,
+            anuncio: false
+        }
+    },
+    methods: {
+        createEventMethod(){
+            console.log(this.createEvent)
+            if(this.createEvent == false){
+                this.createEvent = true,
+                this.adminholder = false,
+                this.anuncio = false
+            }
+            else if(this.createEvent == true){
+                this.createEvent = false,
+                this.adminholder = false,
+                this.anuncio = false
+            }
+        },
+        createAdminHMethod(){
+            if(this.adminholder == false){
+                this.adminholder = true,
+                this.createEvent = false,
+                this.anuncio = false
+            }
+            else if(this.adminholder == true){
+                this.adminholder = false,
+                this.createEvent = false,
+                this.anuncio = false
+            }
+        },
+        createAnuncio(){
+            if(this.anuncio == false){
+                this.anuncio = true,
+                this.createEvent = false,
+                this.adminholder = false
+            }
+            else if(this.anuncio == true){
+                this.anuncio = false,
+                this.createEvent = false,
+                this.adminholder = false
+            }
+        }
+    }
+}
+</script>
