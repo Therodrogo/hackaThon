c24953199b96e6927a5b80af23d7fa664a02aab3
LuisMedinaBravo <luis.medina.bravo@hotmail.com>
2022-07-07 22:25:27 -0400

    Vista Anuncio creada y linkeados los anuncios
    Se creó y estilizó la vista de los anuncios, y se dejaron linkeados los anuncios de la vista principal, además, se le dio un orden para el título, imagen y descripción.

--- frontend/components/BodyInformation.vue
+++ frontend/components/BodyInformation.vue
@@ -6,50 +6,69 @@
              </v-row>
             <v-row>
                 <v-col
+
+                
+
                     cols="12"
                     md="4"
-                >
+                >  
+                           
+                    <nuxt-link to="/Anuncio">
                     <v-skeleton-loader
                     v-bind="attrs"
                     type="card-avatar, article, actions"
                     ></v-skeleton-loader>
+                    </nuxt-link>
 
+                    <nuxt-link to="/Anuncio">
                     <v-skeleton-loader
                     v-bind="attrs"
                     type="date-picker"
                     ></v-skeleton-loader>
+                    </nuxt-link>
                 </v-col>
 
                 <v-col
                     cols="12"
                     md="4"
                 >
+                    <nuxt-link to="/Anuncio">
                     <v-skeleton-loader
                     v-bind="attrs"
                     type="article, actions"
                     ></v-skeleton-loader>
+                    </nuxt-link>
 
+                   <nuxt-link to="/Anuncio">
                     <v-skeleton-loader
                     v-bind="attrs"
                     type="table-heading, list-item-two-line, image, table-tfoot"
                     ></v-skeleton-loader>
+                     </nuxt-link>
+
                 </v-col>
 
                 <v-col
                     cols="12"
                     md="4"
                 >
+                <nuxt-link to="/Anuncio">
                     <v-skeleton-loader
                     v-bind="attrs"
                     type="list-item-avatar, divider, list-item-three-line, card-heading, image, actions"
                     ></v-skeleton-loader>
-
+                </nuxt-link>
+                
+                <nuxt-link to="/Anuncio">
                     <v-skeleton-loader
                     v-bind="attrs"
                     type="list-item-avatar-three-line, image, article"
                     ></v-skeleton-loader>
+                  </nuxt-link>
+                  
                 </v-col>
             </v-row>
+           
         </v-layout>
   </v-container>
   </div>
---
+++ frontend/pages/Anuncio.vue
@@ -0,0 +1,48 @@
+<template>
+   <div class="container" xs6> 
+
+    <v-row class="text-center" >
+      <v-col><h2 class="text-h4 text-sm-h3 font-weight-black mb-4"> 
+        <br> <br> 
+        Anuncio
+        </h2>
+      </v-col>
+    </v-row>
+   <v-layout >
+    <v-row class="container text-center container--fluid">
+      <!-- 1 -->
+      <v-col>
+        <v-hover v-slot="{ hover }" close-delay="100">
+            <v-card
+              :elevation="hover ? 10 : 1"
+              :class="{ 'on-hover': hover }"
+            >
+         <v-flex class="py-12 px-6 v-sheet v-sheet--outlined v-sheet--shaped theme--light">
+      
+          <div class="text-center">
+            Título
+            <zoom-on-hover 
+              >
+            <v-avatar size="400" color="red">
+              
+            <v-icon  dark size="100">
+                Imagen
+            </v-icon>
+            
+            </v-avatar>
+            </zoom-on-hover>
+            Descripción
+          </div>
+          <v-card-text class="card">
+          <p class="text-h6 text-uppercase font-weight-black mt-6 mb-2" >
+          </p>
+          <p class="text--disabled text-uppercase"></p>
+        </v-card-text>
+       </v-flex >
+       </v-card>
+        </v-hover>
+      </v-col>
+    </v-row>
+   </v-layout>
+  </div> 
+</template>
