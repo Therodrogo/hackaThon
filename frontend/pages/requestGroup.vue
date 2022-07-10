546869232e49180b2d5a88503a93369e4e6c7308
Claudio <cclaudiohenriquez@gmail.com>
2022-07-10 16:35:14 -0400

    avance en requestGroup

--- frontend/pages/requestGroup.vue
+++ frontend/pages/requestGroup.vue
@@ -5,34 +5,38 @@
             <v-container>
                 
         <v-row>
+
             <v-col
             lg="7"
             md="7"
             sm="7"
             xs="7">
-            
+                <v-col align="center">
+                    <v-text-field
+                        prepend-icon='mdi-card-search-outline'
+                        label='Buscar Solicitudes'
+                        v-model="findrequest"
+                    class="v-text-field"
+                    ></v-text-field>
+                </v-col>
                 <v-sheet
                 left min-height="100%"
-                
-                rounded="lg"
-                color="#00CCB1">
+                rounded="lg">
                     
                     <v-list-item>
                         <v-list-item-content>
                             <v-list-item-title>
                                 Solicitudes pendientes
                             </v-list-item-title >
-                            <v-list-item-group mandatory color="#00CCB1">
-                                <v-list-item >
-                                    <v-btn left min-width="70%">RAUL</v-btn>
-                                    <v-btn class=' ml-5 mr-5' color="#00ff00">aceptar</v-btn>
-                                    <v-btn color="#ff0000">denegar</v-btn>
+                            <v-list-item-group  mandatory color="#00CCB1" >
+                                <v-list-item v-for="(n,i) in FilterRequest" :key="i" >
+                                    <v-btn @click="SelectedReq(n)"  text min-width="50%" mx-auto>
+                                        {{n.name}}
+                                    </v-btn>
+                                    <v-btn @click="AcceptRequest(n)" class=' ml-5 mr-5' color="#00ff00">aceptar</v-btn>
+                                    <v-btn @click="DeclineRequest(n)" color="#ff0000">denegar</v-btn>
                                 </v-list-item>
-                                <v-list-item >
-                                    <v-btn left min-width="70%">PEDRO</v-btn>
-                                    <v-btn class=' ml-5 mr-5' color="#00ff00">aceptar</v-btn>
-                                    <v-btn color="#ff0000">denegar</v-btn>
-                                </v-list-item>
+
                                 
                             </v-list-item-group>
                         </v-list-item-content>
@@ -44,14 +48,24 @@
                 height="500"
                 rounded="lg"
                 color="#00CCB1">
-                    <v-list-item>
-                        <v-list-item-content>
-                            <v-list-item-title >
-                                descripcion
-                            </v-list-item-title >
-                        </v-list-item-content>
-                    </v-list-item>
-                    
+                    <template v-slot>
+                        <span
+                        v-for="(n,f) in Request"
+                        :key="f">
+                        <p v-if="n.name == selectedRequest" >
+                            <v-card
+                            class="mx-auto"
+                            max-width="700">
+                            <v-card-title >
+                                Nombre: {{n.name}}
+                            </v-card-title>
+                            <v-card-text class="text--primary">
+                                Evento: {{n.description}}
+                            </v-card-text>
+                            </v-card>
+                        </p>
+                        </span>
+                    </template>
                 </v-sheet>
             </v-col>
         </v-row>
@@ -70,21 +84,70 @@ import API from '~/api'
 import Swal from 'sweetalert2';
 import {usuarioStore} from "../store/index.js"
 import {usuarioActivo} from "../store/index.js"
+import {eventStore} from "../store/index.js"
 const userStore = usuarioStore()
 const activeStore = usuarioActivo()
+const eventoStore = eventStore()
 
   export default {
     data () {
       return {
+        first: false,
+        findrequest:"",
+        selectedRequest: "",
+        Request:[
 
-      }
-    },
-
-    methods: {
-    }, beforeMount() {
+        ],
     }
+    },
+    computed:{
+      FilterRequest(){
+        return this.Request.filter(newRequest =>{
 
+          return newRequest.name.toLowerCase().trim().match(this.findrequest.toLowerCase().trim());
+        })
+      },
+    },
+    methods: {
+        SelectedReq(n){
+        this.selectedRequest = n.name
+        },
+      async getEventByID(){
+      try {
+        const cosa = await API.getRequestGroup({groupID: "62c2640240358d98c83c72e1"})
+        //const cosa = await API.getAllGroups();
+
+        console.log(cosa.data);
+        cosa.data.forEach  (async n => {
+            const local = await API.getUserByID(n.postulant);
+            this.Request.push({_id: n._id,postulant: n.postulant,name: local.data.name,description: n.description});
+        });
+
+
+        
+
+      } catch (error) {
+        console.log(error)
+      }
+      },
+      async AcceptRequest(data){
+        await API.acceptRequest(data);
+        console.log(data)
+      },
+
+      async DeclineRequest(data){
+        await API.declineRequest(data)
+        console.log(data)
+      }
+
+
+
+
+    }, beforeMount() {
+      this.getEventByID()
+    }
   }
+  
 
 
 
