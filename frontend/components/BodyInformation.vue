9afb12042c9cad3af696efa90fa1d93639656b3b
Serllet <fuentesserllet@gmail.com>
2022-07-11 20:19:41 -0400

    BodyLanding anuncios

--- frontend/components/BodyInformation.vue
+++ frontend/components/BodyInformation.vue
@@ -1,11 +1,79 @@
 <template>
-  <div>
-    <v-container>
-        <v-layout>
-           
-        </v-layout>
-  </v-container>
-  </div>
+<div class="container" >
+  <v-layout row wrap>
+
+    <v-flex xs12 sm6>
+    <v-card
+      v-for="(item, i, index) in posts" v-if="i < total/2"
+      :key="item">
+
+      <v-card-text>
+        <div></div>
+        <p class="text-h4 text--primary">
+          {{item.title}}
+        </p>
+        <p>{{item.date}}</p>
+        <div class="text--primary">
+          {{item.description}}
+        </div>
+      </v-card-text>
+
+        <div>
+         <img
+            :src="item.imageUrl"
+            >
+        </div>
+
+      <v-card-actions>
+        <v-btn
+          text
+          color= #CC7C0A
+          @click="reveal = true"
+        >
+          Leer más
+        </v-btn>
+        </v-card-actions>
+    </v-card>
+    </v-flex>
+
+    <v-flex xs12 sm6>
+    <v-card
+      v-for="(item, i, index) in posts" v-if="i > total/2"
+      :key="item">
+
+      <v-card-text>
+        <div></div>
+        <p class="text-h4 text--primary">
+          {{item.title}}
+        </p>
+        <p>{{item.date}}</p>
+        <div class="text--primary">
+          {{item.description}}
+        </div>
+      </v-card-text>
+
+        <div>
+         <img
+            :src="item.imageUrl"
+            >
+        </div>
+
+      <v-card-actions>
+        <v-btn
+          text
+          color= #CC7C0A
+          @click="reveal = true"
+        >
+          Leer más
+        </v-btn>
+        </v-card-actions>
+
+
+    </v-card>
+    </v-flex>
+
+  </v-layout>
+</div>
 </template>
 
 <!-- Script del ciclo temporal para cada -->
@@ -15,11 +83,7 @@ import { onBeforeMount } from 'vue-demi';
 import API from '~/api';
  export default {
     data: () => ({
-      attrs: {
-        class: 'mb-6',
-        boilerplate: true,
-        elevation: 2,
-      },
+      total: 0,
       posts: {
 
       }
@@ -30,6 +94,8 @@ import API from '~/api';
       const post = await API.getAllPosts()
       this.posts = post.data
       console.log(this.posts)
+      this.total= this.posts.length
+      console.log(this.total)
     },
     }, beforeMount() {this.getAllPosts()}
   }
@@ -37,8 +103,17 @@ import API from '~/api';
     
 </script>
 
-<style>
-
-
+<style  scoped>
+.container{
+  text-align: center;
+  padding: 3% 1% 1% 1%;
+}
+img{
+    width:50%;
+    max-width:200px;
+}
+.v-card{
+  margin: 20px;
+}
 
 </style>
--- frontend/components/BodyLanding.vue
+++ frontend/components/BodyLanding.vue
@@ -2,19 +2,22 @@
   <v-carousel
     cycle
     show-arrows-on-hover>
+
+
     <v-carousel-item
       v-for="(item,i) in posts"
-      :key="4"
+      :key="item"
       :src="item.imageUrl"
     >
-        <v-row
-          class="fill-height"
-          justify="center"
-        >
-          <div class="text-h2">
-            {{ item.title }} 
-          </div>
-        </v-row>
+    
+   <v-row
+      class="fill-height"
+      justify="center"
+      
+    >
+    <p >{{ item.title }} </p>
+        
+    </v-row>  
 
     </v-carousel-item>
   </v-carousel>
@@ -55,5 +58,17 @@ import API from '~/api';
   display: inline-block;
   width: 100%;
   text-align: center;
+}
+.v-row{
+  margin-top: 300px;
+}
+
+p{
+  position: relative;
+  top: 100px;
+  font-size: 200%;
+  color: aliceblue;
+  text-shadow: 2px 0 0 rgb(0, 0, 0), -2px 0 0 rgb(0, 0, 0), 0 2px 0 rgb(0, 0, 0), 0 -2px 0 rgb(0, 0, 0), 1px 1px rgb(0, 0, 0), -1px -1px rgb(0, 0, 0), 1px -1px 0 rgb(0, 0, 0), -1px 1px 0 rgb(0, 0, 0);
+  color: #fff
 }
 </style>
